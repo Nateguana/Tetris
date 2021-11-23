9030d0957f19accb26098606314292ed76653fcb
Cierra <cc362x@gmail.com>
2021-11-23 14:31:34 -0500

    may have successfully added joystick controls; untested

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -8,7 +8,7 @@ public class TitleScreen : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown(KeyCode.JoystickButton0))
         {
             SceneManager.LoadScene(1);
         }
