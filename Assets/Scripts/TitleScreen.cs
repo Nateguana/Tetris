c67b8d5c91574270a709a758c455bc1fe9a1203e
Cierra <cc362x@gmail.com>
2021-11-23 14:33:21 -0500

    fixed compile error

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -8,7 +8,7 @@ public class TitleScreen : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown(KeyCode.JoystickButton0))
+        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("joystick button 0"))
         {
             SceneManager.LoadScene(1);
         }
