851bb26ded0a5e2bb3d32ae8043afe205c765266
Cierra <cc362x@gmail.com>
2021-11-24 00:55:05 -0500

    Set minimum slider value to nonzero number

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Slider slider;
+
+    void Start()
+    {
+        float volume;
+        bool result = audioMixer.GetFloat("Volume", out volume);
+        slider.value = Mathf.Pow(10, volume) / 20.0f;
+    }
 
     public void SetVolume (float volume)
     {
