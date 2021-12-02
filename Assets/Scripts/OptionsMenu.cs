d2a9fd5e25c35132bdc99483579f9c6546425931
Cierra <cc362x@gmail.com>
2021-12-02 05:45:08 -0500

    added keyboard & joystick functionality to menus

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,13 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.EventSystems;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public GameObject volumeSlider;
+    public GameObject backButton;
+
+    private int numOfButtons = 2;
+    private int selectedButton;
+    private bool previousAxisState;
+
+    void Start ()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(volumeSlider);
+        selectedButton = 1;
+        previousAxisState = false;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || GetNegAxisDown("Vertical"))
+        {
+            selectedButton += 1;
+            if (selectedButton > numOfButtons)
+            {
+                selectedButton = 1;
+            }
+
+            EventSystem.current.SetSelectedGameObject(null);
+
+            switch (selectedButton)
+            {
+                case 1:
+                    EventSystem.current.SetSelectedGameObject(volumeSlider);
+                    break;
+                case 2:
+                    EventSystem.current.SetSelectedGameObject(backButton);
+                    break;
+            }
+        }
+    }
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20.0f);
+    }
+
+    private bool GetNegAxisDown(string axisName)
+    {
+        bool isAxisPressed = Input.GetAxis(axisName) < 0;
+
+        if (isAxisPressed && previousAxisState)
+        {
+            return false;
+        }
+
+        previousAxisState = isAxisPressed;
+
+        return isAxisPressed;
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
@@ -9,7 +10,17 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+    public GameObject resumeButton;
+    public GameObject menuButton;
+    public GameObject optionsButton;
+    public GameObject creditsButton;
+    public GameObject versionNotesButton;
     public bool scrollingText = false;
+
+    private int numOfButtons = 5;
+    private int selectedButton;
+    private bool previousAxisState;
 
     void Start()
     {
@@ -19,8 +30,8 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) || 
-            (Input.GetJoystickNames().Any() && Input.GetKeyDown(KeyCode.JoystickButton7)))
+        if (!optionsMenuUI.activeSelf && (Input.GetKeyDown(KeyCode.P) || 
+            (Input.GetJoystickNames().Any() && Input.GetKeyDown(KeyCode.JoystickButton7))))
         {
             if (scrollingText)
             {
@@ -36,6 +47,39 @@ public class PauseMenu : MonoBehaviour
                 Pause();
             }
         }
+
+        if (GameIsPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow) || GetNegAxisDown("Vertical"))
+            {
+                selectedButton += 1;
+                if (selectedButton > numOfButtons)
+                {
+                    selectedButton = 1;
+                }
+
+                EventSystem.current.SetSelectedGameObject(null);
+
+                switch (selectedButton)
+                {
+                    case 1:
+                        EventSystem.current.SetSelectedGameObject(resumeButton);
+                        break;
+                    case 2:
+                        EventSystem.current.SetSelectedGameObject(menuButton);
+                        break;
+                    case 3:
+                        EventSystem.current.SetSelectedGameObject(optionsButton);
+                        break;
+                    case 4:
+                        EventSystem.current.SetSelectedGameObject(creditsButton);
+                        break;
+                    case 5:
+                        EventSystem.current.SetSelectedGameObject(versionNotesButton);
+                        break;
+                }
+            }
+        }
     }
 
     public void Init()
@@ -43,6 +87,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        previousAxisState = false;
     }
 
     public void Resume()
@@ -56,6 +101,9 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
+        selectedButton = 1;
         AudioManager.Play("Click2Placeholder"); // Placeholder
     }
 
@@ -78,5 +126,19 @@ public class PauseMenu : MonoBehaviour
         Init();
         SceneManager.LoadScene(4);
         AudioManager.Play("Click3Placeholder"); // Placeholder
+    }
+
+    private bool GetNegAxisDown(string axisName)
+    {
+        bool isAxisPressed = Input.GetAxis(axisName) < 0;
+
+        if (isAxisPressed && previousAxisState)
+        {
+            return false;
+        }
+
+        previousAxisState = isAxisPressed;
+
+        return isAxisPressed;
     }
 }
