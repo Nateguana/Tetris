036a031463a2c0bf535abeeb60f839dad5bf48f4
Nateguana <nateguana@gmail.com>
2021-12-02 20:33:23 -0500

    put scores on leaderboard
    - Added backend code to put scores on leaderboard

--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -12,34 +12,44 @@ public class LeaderBoard : ScrollText
     protected override void Start()
     {
         txt = GetComponent<TMPro.TMP_Text>();
-        StartCoroutine(GetRequest(URL));
+        StartCoroutine(Mathx.GetRequest(URL, makeBoard, ()=>txt.text = "Network Error"));
     }
-    private IEnumerator GetRequest(string uri)
+    private void makeBoard(string text)
     {
-        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        Score[] scores = JsonConvert.DeserializeObject<Score[]>(text);
+        txt.text = "Top 100 Leaderboard:";
+        for (int j = 0; j < scores.Length; j++)
         {
-            yield return request.SendWebRequest();
-            if(request.result != UnityWebRequest.Result.Success)
-            {
-                txt.text = "Network Error";
-                yield break;
-            }
-            string text = request.downloadHandler.text;
-            Score[] scores = JsonConvert.DeserializeObject<Score[]>(text);
-            txt.text = "Top 100 Leaderboard:";
-            for (int j = 0; j < scores.Length; j++)
-            {
-                txt.text += $"\n{j+1}: {scores[j].name} got {scores[j].score} points on {ConvertTime(scores[j].time)}";
-            }
+            txt.text += $"\n{j + 1}: {scores[j].name} got {scores[j].score} points on {ConvertTime(scores[j].time)}";
         }
     }
-    private static string ConvertTime(ulong time) =>
+    private static string ConvertTime(ulong? time) =>
            DateTimeOffset.FromUnixTimeMilliseconds((long)time).Date.ToShortDateString();
+
+    public static void SendScore(MonoBehaviour that, Score score, Action<string> callback)
+    {
+        string data = JsonConvert.SerializeObject(score);
+        that.StartCoroutine(Mathx.PutRequest(URL, data, e =>{
+            callback($"You got slot {JsonConvert.DeserializeObject<Place>(e).place} on the leaderboard");
+        }, () => callback("Network Error")));
+    }
     [Serializable]
-    public class Score
+    public struct Score
     {
         public ulong score;
         public string name;
-        public ulong time;
+        public ulong? time;
+
+        public Score(ulong score, string name)
+        {
+            this.score = score;
+            this.name = name;
+            time = null;
+        }
+    }
+    [Serializable]
+    public struct Place
+    {
+        public int place;
     }
 }
